effef936b8eb37f507736bb4ea5d07554e04e41a
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-06-20 15:06:40 -0300

    migration completed, 3 tables generated

--- server/src/database/migrations/01_create_items.ts
+++ server/src/database/migrations/01_create_items.ts
@@ -1,9 +1,14 @@
 import Knex from 'knex';
 
 export async function up(knex: Knex) {
-
+  return knex.schema.createTable('items', table => {
+    table.increments('id').primary();
+    table.string('image').notNullable();
+    table.string('title').notNullable();
+  })
 }
 
 export async function down(knex: Knex) {
+  knex.schema.dropTable('items');
 
 }
