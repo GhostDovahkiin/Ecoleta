38a6cffd6c158dd62be995088e715528fb5359a1
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-06-20 15:01:55 -0300

    Table created, script on pkg.json and useNullAsDefault

--- server/src/database/migrations/00_create_points.ts
+++ server/src/database/migrations/00_create_points.ts
@@ -1,7 +1,7 @@
 import Knex from 'knex';
 
 export async function up(knex: Knex) {
-  knex.schema.createTable('point', table => {
+  return knex.schema.createTable('point', table => {
     table.increments('id').primary();
     table.string('image').notNullable();
     table.string('name').notNullable();
