eda818a64edaf44e23e0dad237a81962a4a985c1
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-06-20 15:29:27 -0300

    GET items and ts-node-dev script resolved

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 const routes = express.Router();
+import knex from './database/connection';
 
 routes.get('/', (req, res) => {
   res.status(200).json({
@@ -7,6 +8,19 @@ routes.get('/', (req, res) => {
     by: 'Pedro Henrique',
     repo: 'https://github.com/ghostdovahkiin/Ecoleta'
   });
+});
+
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
+  const serializedItems = items.map(item => {
+    return {
+      name: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return res.status(200).json(serializedItems);
+
 });
 
 export default routes;
