4b7d9326036259fc22721478ce6159bf361204e9
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-06-22 18:35:02 -0300

    estado e imutabilidade

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './Header';
+
+// JSX/TSX: Sintaxe de JavaScript ou TypeScript dentro da aplicação
 
 function App() {
+  let [counter, setCounter] = useState(10);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <Header title="MIAU"></Header>
+      <h1>{counter}</h1>
+      <h1>{counter * 2}</h1>
+      <button type='button' onClick={handleButtonClick}>AUMENTAR</button>
+    </div>
   );
 }
 
