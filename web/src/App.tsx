27940b016c245d1a2c5ba76a9be2b8377e300e83
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-06-22 18:52:48 -0300

    started index

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
-import Header from './Header';
+import Home from './pages/home';
 
 // JSX/TSX: Sintaxe de JavaScript ou TypeScript dentro da aplicação
 
 function App() {
-  let [counter, setCounter] = useState(10);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title="MIAU"></Header>
-      <h1>{counter}</h1>
-      <h1>{counter * 2}</h1>
-      <button type='button' onClick={handleButtonClick}>AUMENTAR</button>
-    </div>
+    <Home></Home>
   );
 }
 
