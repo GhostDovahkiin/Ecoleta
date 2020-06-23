e5acf23cf3a4aa78e6b7d2e4f698bcd4e9678389
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-06-22 22:02:48 -0300

    create point header, styles and link to home

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import './styles.css';
+import {Link} from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import { FiArrowLeft } from 'react-icons/fi';
 
 const CreatePoint = () => {
-  return <h1>Miau</h1>
+  return (
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta"/>
+        <Link to="/">
+          <FiArrowLeft></FiArrowLeft>
+          Voltar para home
+        </Link>
+      </header>
+    </div>
+  );
 }
 
 export default CreatePoint;
