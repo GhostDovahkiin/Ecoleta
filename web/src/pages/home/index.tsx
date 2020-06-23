47a1a040c1dde6854d33c6c605fb96a453e181ce
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-06-22 21:13:18 -0300

    home page done

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {FiLogIn} from 'react-icons/fi';
 import './styles.css';
 import logo from '../../assets/logo.svg';
 
@@ -13,6 +14,13 @@ const Home = () => {
         <main>
           <h1>Seu marketplace de coleta de resíduos</h1>
           <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+
+          <a href="/cadastro">
+            <span>
+              <FiLogIn></FiLogIn>
+            </span>
+            <strong>Cadastre um ponto de coleta</strong>
+          </a>
         </main>
       </div>
     </div>
