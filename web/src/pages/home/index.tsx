0c50f64b52cb405c7961e057eddec3084d417399
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-06-22 21:56:15 -0300

    rotas linkadas

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {FiLogIn} from 'react-icons/fi';
+import {Link} from 'react-router-dom';
 import './styles.css';
 import logo from '../../assets/logo.svg';
 
@@ -15,12 +16,12 @@ const Home = () => {
           <h1>Seu marketplace de coleta de resíduos</h1>
           <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
 
-          <a href="/cadastro">
+          <Link to="/create-point">
             <span>
               <FiLogIn></FiLogIn>
             </span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
